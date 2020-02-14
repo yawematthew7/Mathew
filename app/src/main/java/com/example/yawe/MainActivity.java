68aac8387b8bedd0bb5f3c294ebbd72449728181
SCORPIO <buastuartjames@gmail.com>
2020-02-14 12:20:02 +0300

    hello

--- app/src/main/java/com/example/yawe/MainActivity.java
+++ app/src/main/java/com/example/yawe/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.yawe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public  void sendMesaage (View view){
+        EditText message = (EditText)findViewById(R.id.edit_message);
+        Toast.makeText(this, "sending Message" +message.getText().toString(),Toast.LENGTH_SHORT).show();
+
+        Intent intent=new Intent(this,DisplayMessageActivity.class);
+        intent.putExtra("Message",message.getText().toString());
+        startActivity(intent);
+
+        message.setText("");
     }
 }
